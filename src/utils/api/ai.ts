4c92ea0530b26c6ada31861722d9c5e98a04bbc4
Matheus Satelos <matheus.satelos@gmail.com>
2025-03-05 21:24:46 -0300

    add ai endpoint

---
+++ src/utils/api/ai.ts
@@ -0,0 +1,25 @@
+import axios, { isAxiosError } from "axios";
+
+const AI_API_URL = "http://localhost:8080/habot";
+
+interface IAIMessage {
+  message: string
+}
+
+export async function sendAIMessage(value: IAIMessage) {
+  try {
+    const { data } = await axios.post(`${AI_API_URL}`, { question: value.message }, {
+      withCredentials: true,
+    })
+    console.log('value.message', value.message)
+    console.log('data', data)
+    return data;
+  } catch (error: unknown) {
+    console.log(error);
+    if (isAxiosError(error)) {
+      throw new Error(error?.response?.data?.message) || "Something went wrong";
+    } else {
+      throw "An unexpected error occurred";
+    }
+  }
+}
